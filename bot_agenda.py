807ed5f988c6d6975dd968951a940eb145d52e4e
otavio <oalencardev@gmail.com>
2025-03-26 18:36:37 -0300

    feat:sugestoes e start implementados

--- bot_agenda.py
+++ bot_agenda.py
@@ -3,8 +3,26 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from pathlib import Path
 import json
 from datetime import datetime
+from datetime import time
+import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Olá! Eu sou seu bot")
+    mensagem = f"""
+    Olá eu sou o {os.getenv('BOT_NAME')}, seja bem vindo!
+
+    Aqui está a lista de comandos para você utilizar:
+    ➤ /start - Mostra os comandos
+    ➤ /hello - Retorna um olá mundo
+    ➤ /agendar  *data *hora *duração - Adiciona um agendamento
+    ➤ /consultar  *data - Mostra os seus agendamentos
+    ➤ /cancelar  *data *hora - Cancela um agendamento existente
+    ➤ /sugerir  *data *duração - Sugere um agendamento
+
+"""
+    await update.message.reply_text(mensagem)
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Olá, Mundo! 🚀")
@@ -79,16 +97,42 @@ async def cancelar(update: Update, context: ContextTypes.DEFAULT_TYPE):
     with open("agendamentos.txt", "w", encoding="utf-8") as file:
         for agendamento in sem_cancelados:
             file.write(json.dumps(agendamento) + "\n") 
-    await update.message.reply_text(f"Agendamento de {data} às {hora} cancelado com sucesso! ✅")    
+    await update.message.reply_text(f"Agendamento de {data} às {hora} cancelado com sucesso!")    
+
+
+async def sugerir(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    agendamentos = []
+    arquivo = Path('agendamentos.txt')
+    if not arquivo.exists():
+        await update.message.reply_text("Não há nenhum agendamento registrado.")
+        return   
+    
+    with open("agendamentos.txt", "r",encoding='utf-8') as linhas:
+        for x in linhas:
+            agendamentos.append(json.loads(x.strip()))
+    data = str(context.args[0])
+    duracao = str(context.args[1])
+    hora =  '00:00'
+ 
+    while True:
+        hora =  time(random.randint(7,18),random.randint(0,59)).strftime("%H:%M")
+        lista  = [x for x in agendamentos if hora == x['hora']]
+        if len(lista) > 0:
+            continue
+        else:
+            break
+    await update.message.reply_text(f"Você pode agendar em {data}, às {hora}, com duração de {duracao} minutos") 
+    
 
 def main():
 
-    app = ApplicationBuilder().token("7919020982:AAGYNoGmvsk6aCOtyFqYUBkNz-TZBkVUbbc").build()
+    app = ApplicationBuilder().token(os.getenv('MY_SECRET_TOKEN')).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("hello", hello))
     app.add_handler(CommandHandler("agendar", agendar))
     app.add_handler(CommandHandler("consultar", consultar))
     app.add_handler(CommandHandler("cancelar", cancelar))
+    app.add_handler(CommandHandler("sugerir", sugerir))
     app.run_polling()
 if __name__ == "__main__":
     main()
