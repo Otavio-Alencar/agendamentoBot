d34b195bccda21ccbf46c945ddd9d2301cd0b7d0
otavio <oalencardev@gmail.com>
2025-03-26 17:23:55 -0300

    feat: consulta e cancelamento implementados

--- bot_agenda.py
+++ bot_agenda.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from pathlib import Path
 import json
+from datetime import datetime
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Olá! Eu sou seu bot")
 
@@ -38,9 +39,47 @@ async def agendar(update: Update, context: ContextTypes.DEFAULT_TYPE):
         file.write(json.dumps(objeto) + '\n')
         file.close()
     await update.message.reply_text("Agendamento realizado com sucesso!")
-    
-    
 
+
+async def consultar(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    agendamentos = []
+    arquivo = Path('agendamentos.txt')
+    if not arquivo.exists():
+        await update.message.reply_text("Não há nenhum agendamento registrado.")
+        return
+    with open("agendamentos.txt", "r",encoding='utf-8') as linhas:
+        for x in linhas:
+            agendamentos.append(json.loads(x.strip()))
+    
+    data = str(context.args[0])
+    formato_hora = "%H:%M"
+    selecionados = [x for x in agendamentos if x['data'] == data];
+    por_horario = sorted(selecionados, key=lambda x: datetime.strptime(x['hora'], "%H:%M"))
+    await update.message.reply_text("Seus Agendamentos:\n")
+    for x in por_horario:
+        await update.message.reply_text(f"{x['data']},  {x['hora']}, {x['duracao']}")
+       
+            
+       
+async def cancelar(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    agendamentos = []
+    arquivo = Path('agendamentos.txt')
+    if not arquivo.exists():
+        await update.message.reply_text("Não há nenhum agendamento registrado.")
+        return   
+    
+    with open("agendamentos.txt", "r",encoding='utf-8') as linhas:
+        for x in linhas:
+            agendamentos.append(json.loads(x.strip()))
+
+    data = str(context.args[0])
+    hora = str(context.args[1])
+    sem_cancelados = [a for a in agendamentos if not (a["data"] == data and a["hora"] == hora)]
+    
+    with open("agendamentos.txt", "w", encoding="utf-8") as file:
+        for agendamento in sem_cancelados:
+            file.write(json.dumps(agendamento) + "\n") 
+    await update.message.reply_text(f"Agendamento de {data} às {hora} cancelado com sucesso! ✅")    
 
 def main():
 
@@ -48,6 +87,8 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("hello", hello))
     app.add_handler(CommandHandler("agendar", agendar))
+    app.add_handler(CommandHandler("consultar", consultar))
+    app.add_handler(CommandHandler("cancelar", cancelar))
     app.run_polling()
 if __name__ == "__main__":
     main()
